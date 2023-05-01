369ea5b3e84c3b68c72a703c319220b2ca0d3a6c
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-05-01 23:40:07 +0530

    Add files via upload

---
+++ plugins/ExraMods/check_alive.py
@@ -0,0 +1,18 @@
+import time
+import random
+from pyrogram import Client, filters
+
+CMD = ["/", "."]
+
+@Client.on_message(filters.command("alive", CMD))
+async def check_alive(_, message):
+    await message.reply_text("Hey there! I'm alive and well, and ready to take on the day. Thanks for checking in! Click /start And Enjoy!😊")
+
+
+@Client.on_message(filters.command("ping", CMD))
+async def ping(_, message):
+    start_t = time.time()
+    rm = await message.reply_text("...")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
