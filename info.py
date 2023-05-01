af20d2f711ca4fe231c3168881fff2b80c5440b5
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-05-02 00:19:59 +0530

    Add files via upload

---
+++ info.py
@@ -0,0 +1,87 @@
+import re
+from os import environ
+import asyncio
+import json
+from collections import defaultdict
+from typing import Dict, List, Union
+from pyrogram import Client
+from time import time
+
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.strip().lower() in ["on", "true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.strip().lower() in ["off", "false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+
+
+# Bot information
+PORT = environ.get("PORT", "8080")
+WEBHOOK = bool(environ.get("WEBHOOK", False)) # for web support on/off
+SESSION = environ.get('SESSION', 'Media_search')
+API_ID = int(environ['API_ID'])
+API_HASH = environ['API_HASH']
+BOT_TOKEN = environ['BOT_TOKEN']
+
+# Bot settings
+CACHE_TIME = int(environ.get('CACHE_TIME', 300))
+USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
+PICS = (environ.get('PICS' ,'https://telegra.ph/file/0507c8ee38765648de521.jpg')).split()
+BOT_START_TIME = time()
+
+# Admins, Channels & Users
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+AUTH_USERS = (auth_users + ADMINS) if auth_users else []
+auth_channel = environ.get('AUTH_CHANNEL')
+auth_grp = environ.get('AUTH_GROUP')
+AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
+AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
+
+# MongoDB information
+DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+
+#maximum search result buttos count in number#
+MAX_RIST_BTNS = int(environ.get('MAX_RIST_BTNS', "10"))
+START_MESSAGE = environ.get('START_MESSAGE', '👋 𝙷𝙸𝙸: {user}\n𝙼𝚈𝚂𝙴𝙻𝙵: {bot},\n\n🍿 𝚆𝙴𝙻𝙲𝙾𝙼𝙴 𝚃𝙾 𝚃𝙷𝙴 𝚆𝙾𝚁𝙻𝙳 𝙲𝙾𝙾𝙻𝙴𝚂𝚃 𝚂𝙴𝙰𝚁𝙲𝙷 𝙴𝙽𝙶𝙸𝙽𝙴!\n\n𝚂𝙴𝙽𝙳 𝙰 𝙼𝙴𝚂𝚂𝙰𝙶𝙴 𝚃𝙾 𝚂𝙴𝙰𝚁𝙲𝙷 𝙾𝚁 𝙲𝙻𝙸𝙲𝙺 "𝚂𝙴𝙰𝚁𝙲𝙷" 𝙱𝚄𝚃𝚃𝙾𝙽!...')
+BUTTON_LOCK_TEXT = environ.get("BUTTON_LOCK_TEXT", "⚠️ 𝙃𝙚𝙮 {query}! 𝙏𝙝𝙖𝙩'𝙨 𝙉𝙤𝙩 𝙁𝙤𝙧 𝙔𝙤𝙪. 𝙋𝙡𝙚𝙖𝙨𝙚 𝙍𝙚𝙦𝙪𝙚𝙨𝙩 𝙔𝙤𝙪𝙧 𝙊𝙬𝙣")
+FORCE_SUB_TEXT = environ.get('FORCE_SUB_TEXT', 'Please Join All 3 Updates Channel to use this Bot!')
+RemoveBG_API = environ.get("RemoveBG_API", "taF9K5WQVgN3tdsnjuYhg7EN")
+WELCOM_PIC = environ.get("WELCOM_PIC", "")
+WELCOM_TEXT = environ.get("WELCOM_TEXT", "Hai {user}\nwelcome to {chat}")
+PMFILTER = environ.get('PMFILTER', "True")
+G_FILTER = bool(environ.get("G_FILTER", True))
+BUTTON_LOCK = environ.get("BUTTON_LOCK", "True")
+
+# url shortner
+SHORT_URL = environ.get("SHORT_URL")
+SHORT_API = environ.get("SHORT_API")
+
+# Others
+IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "300"))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'iPepkornBots')
+P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
+PM_IMDB = environ.get('PM_IMDB', "True")
+IMDB = is_enabled((environ.get('IMDB', "True")), True)
+SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "False")), False)
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
+BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", None)
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>Query: {query}</b> \n‌IMDb Data:\n\n🏷 Title: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a> / 10")
+LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
+SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
+MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
+INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
+MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
+PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
+PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
+
+#request force sub
+REQ_SUB = bool(environ.get("REQ_SUB", True))
+SESSION_STRING = environ.get("SESSION_STRING", "")
