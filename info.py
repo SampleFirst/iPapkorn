1e3e08c8d1d44e342bbd674fc91fd0b286fff423
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-05-02 23:19:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 #maximum search result buttos count in number#
