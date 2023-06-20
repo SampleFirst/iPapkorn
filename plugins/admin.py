4c1e5d1e9ebcef1fdebcf6a8573c6448dcf2dcf6
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-06-21 00:43:26 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -1,11 +1,17 @@
-import re, asyncio, time, shutil, psutil, os, sys
+import re
+import asyncio
+import time
+import shutil
+import psutil
+import os
+import sys
 from pyrogram import Client, filters, enums
 from pyrogram.types import *
 from info import BOT_START_TIME, ADMINS
-from utils import humanbytes  
+from utils import humanbytes
 
 
-@Client.on_message(filters.private & filters.command("status") & filters.user(ADMINS))          
+@Client.on_message(filters.private & filters.command("status") & filters.user(ADMINS))
 async def stats(bot, update):
     currentTime = time.strftime("%Hh%Mm%Ss", time.gmtime(time.time() - BOT_START_TIME))
     total, used, free = shutil.disk_usage(".")
@@ -16,18 +22,19 @@ async def stats(bot, update):
     ram_usage = psutil.virtual_memory().percent
     disk_usage = psutil.disk_usage('/').percent
 
-    ms_g = f"""<b><u>Bot Status</b></u>
+    ms_g = f"""<b><u>Bot Status</u></b>
 
 Uptime: <code>{currentTime}</code>
 CPU Usage: <code>{cpu_usage}%</code>
 RAM Usage: <code>{ram_usage}%</code>
 Total Disk Space: <code>{total}</code>
 Used Space: <code>{used} ({disk_usage}%)</code>
-Free Space: <code>{free}</code> """
+Free Space: <code>{free}</code>"""
 
-    msg = await bot.send_message(chat_id=update.chat.id, text="__Processing...__", parse_mode=enums.ParseMode.MARKDOWN)         
+    msg = await bot.send_message(chat_id=update.chat.id, text="__Processing...__", parse_mode=enums.ParseMode.MARKDOWN)
     await msg.edit_text(text=ms_g, parse_mode=enums.ParseMode.HTML)
-   
+
+
 @Client.on_message(filters.command('logs') & filters.user(ADMINS))
 async def log_file(bot, message):
     try:
@@ -38,7 +45,7 @@ async def log_file(bot, message):
 
 @Client.on_message(filters.command("restart") & filters.user(ADMINS))
 async def stop_button(bot, message):
-    msg = await bot.send_message(text="**🔄 𝙿𝚁𝙾𝙲𝙴𝚂𝚂𝙴𝚂 𝚂𝚃𝙾𝙿𝙴𝙳. 𝙱𝙾𝚃 𝙸𝚂 𝚁𝙴𝚂𝚃𝙰𝚁𝚃𝙸𝙽𝙶...**", chat_id=message.chat.id)       
+    msg = await bot.send_message(text="**🔄 𝙿𝚁𝙾𝙲𝙴𝚂𝚂𝙴𝚂 𝚂𝚃𝙾𝙿𝙴𝙳. 𝙱𝙾𝚃 𝙸𝚂 𝚁𝙴𝚂𝚃𝙰𝚁𝚃𝙸𝙽𝙶...**", chat_id=message.chat.id)
     await asyncio.sleep(3)
     await msg.edit("**✅️ 𝙱𝙾𝚃 𝙸𝚂 𝚁𝙴𝚂𝚃𝙰𝚁𝚃𝙴𝙳. 𝙽𝙾𝚆 𝚈𝙾𝚄 𝙲𝙰𝙽 𝚄𝚂𝙴 𝙼𝙴**")
     os.execl(sys.executable, sys.executable, *sys.argv)
