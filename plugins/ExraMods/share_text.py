46dd4d0e528b4ab96792a0aab4d69efe0e806b1e
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-06-21 00:18:35 +0530

    Update share_text.py

--- plugins/ExraMods/share_text.py
+++ plugins/ExraMods/share_text.py
@@ -16,7 +16,7 @@ async def share_text(client, message):
         await message.reply_text(
             text=f"**Notice:**\n\n1. Reply Any Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**",                
             reply_to_message_id=reply_id,               
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Support Chat", url=f"https://t.me/iPepkornBots")]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Support Chat", url=f"https://t.me/iPapkornSupportGroup")]])
             )                                                   
         return
     await message.reply_text(
@@ -24,3 +24,4 @@ async def share_text(client, message):
         reply_to_message_id=reply_id,
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("♂️ Share", url=f"https://t.me/share/url?url={quote(input_text)}")]])       
     )
+    
