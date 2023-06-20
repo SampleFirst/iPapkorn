06fb7a3f3c1cedca146a00b8e417687112931070
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-06-21 01:12:45 +0530

    Update purge.py

--- plugins/group_manage/purge.py
+++ plugins/group_manage/purge.py
@@ -1,31 +1,31 @@
 import asyncio
-from pyrogram import Client, filters, enums 
+from pyrogram import Client, filters, types
 from plugins.helper.admin_check import admin_check
 
 
-@Client.on_message(filters.command("purge") & (filters.group | filters.channel))                   
+@Client.on_message(filters.command("purge") & (filters.group | filters.channel))
 async def purge(client, message):
-    if message.chat.type not in ((enums.ChatType.SUPERGROUP, enums.ChatType.CHANNEL)):
+    if message.chat.type not in (types.ChatType.SUPERGROUP, types.ChatType.CHANNEL):
         return
-    is_admin = await admin_check(message)
+    is_admin = await admin_check(client, message)
     if not is_admin:
         return
 
     status_message = await message.reply_text("...", quote=True)
     await message.delete()
     message_ids = []
-    count_del_etion_s = 0
+    count_deletions = 0
 
     if message.reply_to_message:
-        for a_s_message_id in range(message.reply_to_message.id, message.id):
-            message_ids.append(a_s_message_id)
-            if len(message_ids) == "100":
+        for message_id in range(message.reply_to_message.message_id, message.message_id):
+            message_ids.append(message_id)
+            if len(message_ids) == 100:
                 await client.delete_messages(
                     chat_id=message.chat.id,
                     message_ids=message_ids,
                     revoke=True
                 )
-                count_del_etion_s += len(message_ids)
+                count_deletions += len(message_ids)
                 message_ids = []
         if len(message_ids) > 0:
             await client.delete_messages(
@@ -33,7 +33,7 @@ async def purge(client, message):
                 message_ids=message_ids,
                 revoke=True
             )
-            count_del_etion_s += len(message_ids)
-    await status_message.edit_text(f"deleted {count_del_etion_s} messages")
+            count_deletions += len(message_ids)
+    await status_message.edit_text(f"Deleted {count_deletions} messages")
     await asyncio.sleep(5)
     await status_message.delete()
