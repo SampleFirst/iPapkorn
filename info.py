cb04aa1a5aaa952268a31659202a93f2a2a54514
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-06-21 13:23:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,7 +55,7 @@ RemoveBG_API = environ.get("RemoveBG_API", "taF9K5WQVgN3tdsnjuYhg7EN")
 WELCOM_PIC = environ.get("WELCOM_PIC", "")
 WELCOM_TEXT = environ.get("WELCOM_TEXT", "Hai {user}\nwelcome to {chat}\n\nPlease Join Updates Channel to use this Bot!")
 PMFILTER = is_enabled(environ.get('PMFILTER', "True"), True)
-G_FILTER = is_enabled(environ.get("G_FILTER", True), True)
+G_FILTER = bool(environ.get("G_FILTER", True))
 BUTTON_LOCK = is_enabled(environ.get("BUTTON_LOCK", "True"), True)
 # url shortner
 SHORT_URL = environ.get("SHORT_URL")
